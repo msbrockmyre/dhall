53d8402bf5643a4968453a208aaaa1e28f11dd98
Hanna Pak <hannapak@Hannas-MacBook.local>
2017-05-19 09:21:31 -0400

    made tags an array

--- models/image.js
+++ models/image.js
@@ -6,7 +6,7 @@ var ImageSchema = new Schema({
     title:          { type: String },
     description:    { type: String },
     filename:       { type: String },
-    tags:           { type: String },
+    tags:           { type: Array },
     views:          { type: Number, 'default': 0 },
     likes:          { type: Number, 'default': 0 },
     timestamp:      { type: Date, 'default': Date.now }
